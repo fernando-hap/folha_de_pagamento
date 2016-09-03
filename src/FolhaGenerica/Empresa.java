cb193a33aaebe3b4abf198b5107a31e884aa8c4c
Henrique Jacques Teixeira De Freitas <henrique.jacques@pucpr.edu.br>
2016-09-03 16:14:05 -0300

    outros

--- src/FolhaGenerica/Empresa.java
+++ src/FolhaGenerica/Empresa.java
@@ -4,10 +4,18 @@ public abstract class Empresa {
 	private String nome;
 	
 	public Empresa(String nome) {
-		
+		this.nome = nome;
 	}
 	
 	public void gerarFolha() {
 		
+	}
+	
+	public void exibirFuncionario(int codigo) {
+		
+	}
+	
+	public boolean inserirFuncionario(Funcionario f) {
+		return true;
 	}
 }
---
+++ src/FolhaInformatica/EmpresaInformatica.java
@@ -0,0 +1,12 @@
+package FolhaInformatica;
+
+import FolhaGenerica.Empresa;
+
+public class EmpresaInformatica extends Empresa {
+
+	public EmpresaInformatica(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+
+}
