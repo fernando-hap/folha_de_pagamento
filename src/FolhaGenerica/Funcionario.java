38932f5fbcba02d8d21e7a3c2c51b350fe47da9a
Henrique Jacques Teixeira De Freitas <henrique.jacques@pucpr.edu.br>
2016-09-03 16:21:48 -0300

    geral

--- src/FolhaGenerica/Funcionario.java
+++ src/FolhaGenerica/Funcionario.java
@@ -3,4 +3,23 @@ package FolhaGenerica;
 public abstract class Funcionario {
 	private String nome;
 	private int codigo;
+	
+	public Funcionario(String nome, int codigo) {
+		this.nome = nome;
+		this.codigo = codigo;
+	}
+	
+	void gerarContraCheque() {
+		
+	}
+	
+	protected abstract double salario();
+	
+	protected abstract String cargo();
+	
+	public void exibir() {
+		
+	}
+	
+	
 }
--- src/FolhaGenerica/Mensalista.java
+++ src/FolhaGenerica/Mensalista.java
@@ -2,4 +2,14 @@ package FolhaGenerica;
 
 public abstract class Mensalista extends Funcionario {
 	private double salarioMensal;
+	
+	public Mensalista(String nome, int codigo) {
+		super(nome, codigo);
+	}
+
+	protected double salario() {
+		return salarioMensal;
+	}
+	
+	protected abstract double comissao();
 }
